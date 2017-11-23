e638562483b650cdf9fe91448f8502f40b2bc3cf
bymoxb <bymoxb@10.5.4.36>
2017-11-23 16:38:05 -0500

    Update 1

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -3,11 +3,7 @@ package pruebas;
 public class Pruebas {
 
     public static void main(String[] args) {
-//        for (int i = 2; i < 100; i++) {
-//            if (esPrimo(i)) {
-//                System.out.println(i + " : Es primo");
-//            }
+        System.out.println("HOLA MUNDO");
     }
 
-    
 }
